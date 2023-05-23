d4716abdc14040147daf03e700e801e93819db04
dong-geon-Lee <ch9716@naver.com>
2023-05-23 14:17:51 +0900

    공공데이터 추가 및 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar/Navbar";
 
 const App: React.FC = () => {
+  const [mockData, setMockData] = useState([]);
+
+  console.log(mockData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios("/data/전국평생학습강좌표준데이터.json");
+      setMockData(response.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-5xl text-blue-600">Hello</h1>
+    <div className="w-[100%]">
+      <Navbar />
     </div>
   );
 };
